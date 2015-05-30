27e15e9379e525418e0eeb46d83df4c0652bb223
AaronOngTW <aaronongtw@gmail.com>
2015-05-30 13:30:04 +1000

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,42 @@
 class UsersController < ApplicationController
-  def new
+
+
+  def index
+    @users = User.all
   end
 
-  def edit
+  def new
+    @user = User.new
   end
 
   def create
+    @user = User.new user_params
+      if @user.save
+        redirect_to root_path
+      else
+        render :new
+      end
+  end
+
+  def edit
+    @user = @current_user
+    render :new
+  end
+
+  def update
+    user = @current_user
+    user.update user_params
+    redirect_to root_path
   end
 
   def show
   end
 
-  def destroy
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
-  def update
-  end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@
 
 class User < ActiveRecord::Base
   has_many :databases
+    ##ADDING SECURITY##
+  has_secure_password
+
+  ##NEEDS A NAME##
+  validates :email, :presence => true
+
+  ##useremail is case sensitive
+  validates :email, :uniqueness => true
+
+  ##username of length of 6
+  validates :password, length: { in: 6..20 }
 end
